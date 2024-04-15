b9943dc15f8dfb5c63ac789f88d5931af543e42d
Ovomaltine2019 <zizlavska.e@seznam.cz>
2024-04-15 08:52:49 +0200

    funkce read_data a selection_sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -11,9 +12,39 @@ def read_data(file_name):
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
 
+    data = {}
+    with open(file_path, mode="r", newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        headers = next(csv_reader)
+
+        # pro každý sloupec prázdný seznam v datovém slovníku
+        for header in headers:
+            data[header] = []
+
+        for row in csv_reader:
+            for i, value in enumerate(row):
+                data[headers[i]].append(float(value))
+
+    return data
+
+
+def selection_sort(seznam):
+    for i in range(len(seznam)):
+        min_idx = i
+        for j in range(i+1, len(seznam)):
+            if seznam[min_idx] > seznam[j]:
+                min_idx = j
+        seznam[i], seznam[min_idx] = seznam[min_idx], seznam[i]
+
+    return seznam
+
 
 def main():
-    pass
+    file_name = 'numbers.csv'
+    data = read_data(file_name)
+    print(data)
+    selection = selection_sort(data['series_1'])
+    print(selection)
 
 
 if __name__ == '__main__':
